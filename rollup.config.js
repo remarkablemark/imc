2036626a107e9672dc0cfe772ccbfffdc8877ae5
Mark <mark@remarkablemark.org>
2020-05-23 15:32:50 -0400

    build(rollup): update rollup.config.js to build min umd
    Enable sourcemap for both min and unmin umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+/**
+ * Build rollup config for development (default) or production (when minify is true).
+ *
+ * @param {Boolean} [minify=false]
+ * @return {Object}
+ */
+const getConfig = (minify = false) => ({
   input: 'src/index.ts',
   output: {
-    file: 'umd/imc.js',
+    file: `umd/imc${minify ? '.min' : ''}.js`,
     format: 'umd',
     name: 'IMC',
+    sourcemap: true,
   },
-  plugins: [typescript()],
-};
+  plugins: [typescript(), minify && uglify()],
+});
+
+export default [getConfig(), getConfig(true)];
